c9b3c6fc54bd67303811ebff7d7c7507011eca9a
Boaz <omeryaari@gmail.com>
2020-01-21 22:13:23 +0200

    ד

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,10 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule} from './shared/material.module';
-import { CombatReadinessComponent } from './components/combat-readiness/combat-readiness.component';
-import { ReccomendationsComponent } from './components/combat-readiness/reccomendations/reccomendations.component';
-
+  
 @NgModule({
   declarations: [
     AppComponent,
-    CombatReadinessComponent,
-    ReccomendationsComponent
   ],
   imports: [
     BrowserModule,
